9dcad70874c0323e77acc0b68e702a22b6dd54f4
Your Name <you@example.com>
2021-07-07 12:24:22 +0530

    Third Commit

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,13 +4,6 @@ import Skills from "./Skills";
 import Footer from "./Footer";
 
 function App() {
-  // const [data, setData] = React.useState(null);
-
-  // React.useEffect(() => {
-  //   fetch("/")
-  //     .then((res) => res.json())
-  //     .then((data) => setData(data.message));
-  // }, []);
   return (
     <div>
     <Cover />
